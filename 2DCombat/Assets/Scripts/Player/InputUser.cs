f1a1fe9ff87918607f6a83a49ab78ba257590cd6
hej7866 <hej7866@naver.com>
2025-01-14 22:59:34 +0900

    (2025.01.14) 패키지 익스포트 공부

---
+++ 2DCombat/Assets/Scripts/Player/InputUser.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using CustomUtility;
+
+public class InputUser : SingleTon<InputUser>
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
